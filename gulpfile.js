026825c5d822455163ab738a519a3a7efbb9a576
Tiberiu Grecu <development@mw-a.net>
2016-08-09 00:03:58 +0300

    Added watch to default

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('compass', function() {
 });
 
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
 
 gulp.task('watch', function() {
